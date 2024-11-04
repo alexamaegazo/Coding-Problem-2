6ff2ed604fbe8c88d7af9e23a10e757e231360d1
alexamaegazo <alexamaegazo5@gmail.com>
2024-11-04 19:10:15 +0800

    Initial commit with classSubject.js

---
+++ classSubject.js
@@ -0,0 +1,8 @@
+//Collect the following user information:
+
+let subjectTitle = "Data Structure";
+let classSched = "8:00- 9:30, Tuesday, Thursday";
+let classroom = "Comlab A"
+let classInstructor = "Sir Barayuga";
+let studentName = "Alexa Mae Gazo";
+console.log(' Alexa Mae Gazo is enrolled in Data Structure with a class schedule of 8:00- 9:30, Tuesday, Thursday, at Comlab A.The instructor for the subject is Sir Barayuga');
